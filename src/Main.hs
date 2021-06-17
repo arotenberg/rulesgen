c662f1618b73b4a863672c400d5b473137e91b96
Aaron Rotenberg <arotenberg@users.noreply.github.com>
2021-06-17 18:43:12 -0400

    Add --count command line arg; update Stackage LTS.

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,45 @@
 module Main(
-    main,
-    mainWithArgs
+    main
 ) where
 
+import Control.Monad(replicateM_)
 import qualified Options.Applicative as Opt
-import System.Environment(getArgs)
 
 import RulesGen.Gen
 import RulesGen.Loader
 import RulesGen.Rules
 
 main :: IO ()
-main = getArgs >>= mainWithArgs
+main = do
+    ra <- Opt.execParser mainInfo
+    rules <- loadRulesFile (raRulesFilePath ra)
+    replicateM_ (raCount ra) $ do
+        resultString <- runRulesGen rules (genFromNonterminal startNT)
+        putStrLn resultString
 
-mainWithArgs :: [String] -> IO ()
-mainWithArgs args = do
-    let parsedOptions = Opt.execParserPure Opt.defaultPrefs mainInfo args
-    rulesFile <- Opt.handleParseResult parsedOptions
-    rules <- loadRulesFile rulesFile
-    resultString <- runRulesGen rules (genFromNonterminal startNT)
-    putStrLn resultString
-
-mainInfo :: Opt.ParserInfo FilePath
-mainInfo = Opt.info (Opt.helper <*> rulesFileParser) (Opt.header mainHeader)
+mainInfo :: Opt.ParserInfo RulesArgs
+mainInfo = Opt.info (Opt.helper <*> rulesArgsParser) (Opt.header mainHeader)
 
 mainHeader :: String
 mainHeader = "rulesgen - generate random text matching a grammar"
 
-rulesFileParser :: Opt.Parser FilePath
-rulesFileParser = Opt.argument Opt.str (Opt.metavar "RULES_FILE")
+data RulesArgs = RulesArgs {
+    raRulesFilePath :: FilePath,
+    raCount :: Int
+  } deriving (Show)
+
+rulesArgsParser :: Opt.Parser RulesArgs
+rulesArgsParser = RulesArgs
+    <$> Opt.argument Opt.str
+            (Opt.metavar "RULES_FILE"
+            <> Opt.help "File containing the input grammar")
+    <*> Opt.option Opt.auto
+            (Opt.long "count"
+            <> Opt.short 'n'
+            <> Opt.metavar "NUM"
+            <> Opt.help "Number of outputs to generate"
+            <> Opt.value 1
+            <> Opt.showDefault)
 
 startNT :: NonterminalID
 startNT = NonterminalID "Start"
